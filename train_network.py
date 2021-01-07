c5e367786139a9341740754671618f21a170294a
Sinhan Kang <47438617+da2so@users.noreply.github.com>
2021-01-07 10:36:20 +0900

    Update train_network.py

--- train_network.py
+++ train_network.py
@@ -105,7 +105,7 @@ def main():
 
     os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
     os.environ['TF2_BEHAVIOR'] = '1'
-    os.environ['CUDA_VISIBLE_DEVICES']= '3'
+    os.environ['CUDA_VISIBLE_DEVICES']= '0'
 
     config = tf.compat.v1.ConfigProto()
     config.gpu_options.per_process_gpu_memory_fraction = 0.8
